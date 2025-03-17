2f3bce9a5b115d7bdb32c84e98b20f2142cec25e
BeldyAdam <beldy.adam@gmail.com>
2025-03-17 23:42:43 +0100

    Regisztracio, Bejelentkezes, Kijelentkezes Kész és Tesztelve, Teszt tábla létrehozva (Post), törölhető

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+    
+        // Jelszó hashelése
+        /* $fields['password'] = bcrypt($fields['password']); */
+    
+        // Felhasználó létrehozása
+        $user = User::create($fields);
+    
+        // Token generálása
+        /* $token = $user->createToken('auth_token')->plainTextToken; */
+        $token = $user->createToken($request->name);
+    
+        return response()->json([
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ]/* , 201 */);
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return ['message' => 'Hibás bejelentkezés.'];
+        }
+
+        $token = $user->createToken($user->name);
+    
+        return response()->json([
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ]/* , 201 */);
+    }
+
+    public function logout(Request $request){
+        $request->user()->tokens()->delete();
+
+        return ['message' => 'Kijelentkezett'];
+    }
+}
