304ab7aa7a065c2bdfb4d30d028ea42994329baa
bHannah <borsikaaa1@gmail.com>
2025-04-02 18:17:22 +0200

    Minden esemény visszaadása egy felhasználóhoz

--- app/Http/Controllers/EsemenyController.php
+++ app/Http/Controllers/EsemenyController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Esemeny;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class EsemenyController extends Controller
 {
-    //
+    public function getUserEvents($user_id){
+        return Esemeny::all()
+        ->where('user_id', '=', $user_id);        
+    }
 }
